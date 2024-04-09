9039d6e1de0004a72a4c7db0885e569ca3b3d5b4
Fercha Pombo <ferchapombo@gmail.com>
2024-04-09 09:18:09 +0000

    applied migrations after adding new fields to the model. adjusted views and templates

--- weather_app/weather/models.py
+++ weather_app/weather/models.py
@@ -6,5 +6,8 @@ class Weather(models.Model):
     temperature = models.FloatField()
     description = models.CharField(max_length=200)
     icon_url = models.URLField()
-
+    feels_like = models.FloatField(default=False)  # Add feels like field
+    temp_min = models.FloatField(default=False)    # Add min temperature field
+    temp_max = models.FloatField(default=False)    # Add max temperature field
+    humidity = models.IntegerField(default=False) 
 
--- weather_app/weather/views.py
+++ weather_app/weather/views.py
@@ -7,34 +7,46 @@ def home(request):
         city = request.POST.get('city')
         result = get_weather(city)
         if result: 
-            icon_url, temperature,description, city, country = result 
+            # Clear existing weather data
+            Weather.objects.all().delete()
+
+            # Save new weather data
+            icon_url, temperature, description, city, country, feels_like, temp_min, temp_max, humidity = result 
             Weather.objects.create(
                 city=city,
                 country=country,
                 temperature=temperature,
                 description=description,
-                icon_url=icon_url
+                icon_url=icon_url,
+                feels_like=feels_like,
+                temp_min=temp_min,
+                temp_max=temp_max,
+                humidity=humidity
             )
     weathers = Weather.objects.all()
     return render(request, 'home.html', {'weathers': weathers})
 
 def get_weather(city):
     API_key = '0ee939522a1dcd8789c86cdb16ac0966'
-    url=f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={API_key}"
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={API_key}"
     res = requests.get(url)
 
     if res.status_code == 404:
         messagebox.showerror("This city only exists in your imagination")
         return None
-    # Parse the response JSon to get the weather information 
+
+    # Parse the response JSON to get the weather information
     weather = res.json()
     icon_id = weather['weather'][0]['icon']
-    temperature = weather['main']['temp'] - 273.15
+    temperature = weather['main']['temp']
     description = weather['weather'][0]['description']
     city = weather['name']
     country = weather['sys']['country']
+    feels_like = weather['main']['feels_like']
+    temp_min = weather['main']['temp_min']
+    temp_max = weather['main']['temp_max']
+    humidity = weather['main']['humidity']
 
-    # get the weather icon URL and all the weather information
-    icon_url = 'https://openweathermap.org/img/wn/{icon_id}10d@2x.png'
-    return (icon_url, temperature, description, city, country)
-
+    # Construct the weather icon URL
+    icon_url = f'https://openweathermap.org/img/wn/{icon_id}.png'
+    return (icon_url, temperature, description, city, country, feels_like, temp_min, temp_max, humidity)
